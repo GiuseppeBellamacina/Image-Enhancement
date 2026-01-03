1f4210f89d13ee060504bb814b4566d39a391b35
danii909 <danielebarbagallo01@gmail.com>
2026-01-03 21:15:48 +0100

    feat(models): add Pix2PixGenerator and PatchGANDiscriminator

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -4,8 +4,9 @@ Neural network models for image enhancement.
 
 from .unet import UNet
 from .unet_residual import UNetResidual
+from .pix2pix import Pix2PixGenerator, PatchGANDiscriminator
 
 # from .dncnn import DnCNN
 # from .autoencoder import Autoencoder
 
-__all__ = ["UNet", "UNetResidual"]  # , 'DnCNN', 'Autoencoder']
+__all__ = ["UNet", "UNetResidual", "Pix2PixGenerator", "PatchGANDiscriminator"]  # , 'DnCNN', 'Autoencoder']

4c0afbd850aee917affb07f7f0bfdb76282d6030
MattiaCampanella <jzmatx6311@gmail.com>
2026-03-28 17:31:14 +0100

    feat: add NAFNet architecture and training notebook for Gaussian noise removal

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -5,6 +5,7 @@ Neural network models for image enhancement.
 from .unet import UNet
 from .unet_residual import UNetResidual
 from .attention_unet import AttentionUNet
+from .nafnet import NAFNet
 from .pix2pix import Pix2PixGenerator, PatchGANDiscriminator
 
 # from .dncnn import DnCNN
@@ -14,6 +15,7 @@ __all__ = [
     "UNet",
     "UNetResidual",
     "AttentionUNet",
+    "NAFNet",
     "Pix2PixGenerator",
     "PatchGANDiscriminator",
 ]  # , 'DnCNN', 'Autoencoder']
